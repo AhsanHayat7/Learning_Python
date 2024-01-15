8bc4fd97ba6e0419fb00a9343d129e64f1b8553f
AhsanHayat7 <ahsanhayat9071@gmail.com>
2024-01-16 03:23:49 +0500

    Fifth class is completed and rename the folder of fourth class

---
+++ 05_Class(list_tuple and list_comprehensive)/class_5.py
@@ -0,0 +1,24 @@
+data_base : list[tuple[str,str]] = [("haseeb",'123'),
+                                    ("daniyal",'345'),
+                                    ("saad",'123')
+                                    ]
+for row in data_base:
+    print(row)
+
+print([(i**2)  for i in range(1,11)]) # that is the reason why we use print
+
+
+# 4.More Loops: All versions of foods.py in this section have avoided using for loops when
+# printing, to save space. Choose a version of foods.py, and write two for loops to print each list
+# of foods.
+
+# foods.py
+
+# List of fruits
+fruits = ['apple', 'banana', 'orange']
+
+# List of vegetables
+vegetables = ['carrot', 'broccoli', 'spinach']
+
+# List of proteins
+proteins = ['chicken', 'beef', 'fish']
