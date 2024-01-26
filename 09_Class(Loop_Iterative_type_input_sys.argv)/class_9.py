e95a0851290ed3d4eebc76afcd5c854e6eb0b245
AhsanHayat7 <ahsanhayat9071@gmail.com>
2024-01-27 00:00:11 +0500

    Nineth Class Completed

---
+++ 09_Class(Loop_Iterative_type_input_sys.argv)/class_9.py
@@ -0,0 +1,14 @@
+import sys
+
+
+print("Line1")
+print("Line2")
+
+ # iterated data types  list[str] 0 = file  1 = **Valuename user define
+print(type(sys.argv))
+
+if sys.argv[1] == "p":
+    print("I will install python ")
+
+
+print(sys.argv) 
